366317dd0966c70ccf1fbe6d513187d6eb8c719f
spencermountain <spencermountain@gmail.com>
2017-02-25 21:22:26 -0500

    yolo

--- test/trie.test.js
+++ test/trie.test.js
@@ -3,7 +3,6 @@ var test = require('tape');
 const Trie = require('../src/pack/trie');
 const trieHard = require('../src/index');
 
-
 test('trie has everything:', function(t) {
   var arr = [
     'cool',
@@ -11,8 +10,8 @@ test('trie has everything:', function(t) {
     'cool hat',
     'cooledomingo',
   ];
-  let trie = new Trie(arr);
-  for (let i = 0; i < arr.length; i++) {
+  var trie = new Trie(arr);
+  for (var i = 0; i < arr.length; i++) {
     var has = trie.isWord(arr[i])
     t.equal(has, true, 'trie has \'' + arr[i] + '\'')
   }
@@ -27,14 +26,55 @@ test('Ptrie has everything:', function(t) {
     'cool hat',
     'cooledomingo',
   ];
-  let str = trieHard.pack(arr)
+  var str = trieHard.pack(arr)
   t.equal(typeof str, 'string', 'packed-string')
 
-  let ptrie = trieHard.unpack(str)
-  for (let i = 0; i < arr.length; i++) {
+  var ptrie = trieHard.unpack(str)
+  for (var i = 0; i < arr.length; i++) {
     var has = ptrie.isWord(arr[i])
     t.equal(has, true, 'trie has \'' + arr[i] + '\'')
   }
   t.equal(ptrie.isWord('farmington'), false, 'no-false-positive')
+  t.end();
+});
+
+test('uncompressable-array', function(t) {
+  var arr = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+  ];
+  var str = trieHard.pack(arr)
+  var ptrie = trieHard.unpack(str)
+  for (var i = 0; i < arr.length; i++) {
+    var has = ptrie.isWord(arr[i])
+    t.equal(has, true, 'trie has \'' + arr[i] + '\'')
+  }
+  t.equal(ptrie.isWord('z'), false, 'no-false-positive')
+  t.end();
+});
+
+test('very-compressable-array', function(t) {
+  var arr = [
+    'aaa',
+    'aa',
+    'aaa',
+    'aaa',
+    'aaab',
+    'a',
+    'aaaa',
+    'b',
+  ];
+  var str = trieHard.pack(arr)
+  var ptrie = trieHard.unpack(str)
+  for (var i = 0; i < arr.length; i++) {
+    var has = ptrie.isWord(arr[i])
+    t.equal(has, true, 'trie has \'' + arr[i] + '\'')
+  }
+  t.equal(ptrie.isWord('zaaa'), false, 'no-false-positive')
+  t.equal(ptrie.isWord('z aaa'), false, 'no-false-positive-two-word')
   t.end();
 });
