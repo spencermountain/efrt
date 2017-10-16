ab3b8aa190a7343cd6c004b87bcb40ac4aafac0a
spencermountain <spencermountain@gmail.com>
2017-10-16 10:40:30 -0400

    publish efrt-unpack too

--- unpackLib/quick.test.js
+++ unpackLib/quick.test.js
@@ -2,7 +2,7 @@ var unpack = require('./efrt-unpack.min.js');
 var test = require('tape');
 
 test('unpack works:', function(t) {
-  var str = '{"true":"denmark,o0scandanavia;h0ntar0;io"}';
+  var str = 'true¦denmark,o0scandanavia;h0ntar0;io';
   var p = unpack(str);
   t.equal(p.hasOwnProperty('ohio'), true, 'has ohio');
   t.equal(p.hasOwnProperty('ontario'), true, 'has ontario');
