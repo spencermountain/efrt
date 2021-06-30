723dc83e5700cbd4b413381608b82022ed41a24c
spencer kelly <spencerkelly86@gmail.com>
2021-06-30 13:59:56 -0400

    2.3.0rc

--- tests/_lib.js
+++ tests/_lib.js
@@ -1,6 +1,5 @@
-import * as build from '../builds/efrt.js'
+import * as build from '../builds/efrt.mjs'
 import * as src from '../src/index.js'
-
 let lib = src
 if (process.env.TESTENV === 'prod') {
   console.log('== production build test 🚀 ==')
